8b4ade0675aa5d497cbd469a85f59b1d19b09239
Lachlan Grose <lachlan.grose@monash.edu>
2024-01-31 12:01:37 +1100

    fix: letting docs build run on docker

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,9 +57,18 @@ extensions = [
     # add sphinx gallery
     "sphinx_gallery.gen_gallery",
     # citations
+    "myst_parser",
 ]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
+
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

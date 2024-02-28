1abf2d12aa067ff58b359357d32ed06e0fede5e4
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-02-28 16:13:16 +1100

    feat: added dip interpolator and normal vector interpolator

--- map2loop/interpolator.py
+++ map2loop/interpolator.py
@@ -3,6 +3,7 @@ from typing import Tuple, Any
 
 import beartype
 import pandas
+import geopandas
 import numpy
 import math
 from scipy.interpolate import Rbf
@@ -37,7 +38,7 @@ class Interpolator(ABC):
 
     @beartype.beartype
     @abstractmethod
-    def setup_interpolation(self, contact_orientations: pandas.DataFrame, map_data: MapData) -> pandas.DataFrame:
+    def setup_interpolation(self, map_data: MapData) -> Tuple[pandas.DataFrame, list, numpy.ndarray]:
         """
         abstract method to setup interpolation (abstract method)
 
@@ -61,7 +62,7 @@ class Interpolator(ABC):
 
     @beartype.beartype
     @abstractmethod
-    def interpolator(self,x: float, y: float, ni: float, xi: float, yi: float)-> Tuple[numpy.ndarray, list]:
+    def interpolator(self, x: float, y: float, ni: float, xi: float, yi: float) -> Tuple[numpy.ndarray, list]:
         """
         Interpolator method
 
@@ -80,7 +81,7 @@ class Interpolator(ABC):
 
     @beartype.beartype
     @abstractmethod
-    def interpolate(self, contact_orientations: pandas.DataFrame, map_data: MapData) -> list:
+    def interpolate(self, map_data: MapData) -> list:
         """
         Execute interpolate method (abstract method)
 
@@ -96,9 +97,9 @@ class Interpolator(ABC):
         pass
 
 
-class IDWInterpolator(Interpolator):
+class NormalVectorInterpolator(Interpolator):
     """
-    Inverse Distance Weighting interpolation class
+    Normal vector interpolation class
 
     Args:
         Interpolator(ABC): Derived from Abstract Base Class
@@ -121,7 +122,7 @@ class IDWInterpolator(Interpolator):
 
     @beartype.beartype
     @abstractmethod
-    def setup_interpolation(self, contact_orientations: pandas.DataFrame, map_data: MapData) -> pandas.DataFrame:
+    def setup_interpolation(self, map_data: MapData) -> pandas.DataFrame:
         """
         Setup the interpolation method (abstract method)
 
@@ -131,7 +132,7 @@ class IDWInterpolator(Interpolator):
             map_data (map2loop.MapData): a catchall so that access to all map data is available
         """
         # the following code is from LoopStructural
-        contact_orientations = contact_orientations.copy()
+        contact_orientations = map_data.raw_data[1].copy()
         if (
                 "nx" not in contact_orientations.columns
                 or "ny" not in contact_orientations.columns
@@ -182,15 +183,32 @@ class IDWInterpolator(Interpolator):
                 ):
                     contact_orientations["X"] = contact_orientations["easting"]
                 if (
+                        "X" not in contact_orientations.columns
+                        and "easting" not in contact_orientations.columns
+                ):
+                    contact_orientations["X"] = contact_orientations['geometry'].apply(lambda geom: geom.x)
+
+                if (
                         "Y" not in contact_orientations.columns
                         and "northing" in contact_orientations.columns
                 ):
                     contact_orientations["Y"] = contact_orientations["northing"]
                 if (
+                        "Y" not in contact_orientations.columns
+                        and "northing" not in contact_orientations.columns
+                ):
+                    contact_orientations["Y"] = contact_orientations['geometry'].apply(lambda geom: geom.y)
+                if (
                         "Z" not in contact_orientations.columns
                         and "altitude" in contact_orientations.columns
                 ):
                     contact_orientations["Z"] = contact_orientations["altitude"]
+                if (
+                        "Z" not in contact_orientations.columns
+                        and "altitude" not in contact_orientations.columns
+                ):
+                    contact_orientations["Z"] = 0
+
                 if (
                         "X" not in contact_orientations.columns
                         or "Y" not in contact_orientations.columns
@@ -247,7 +265,7 @@ class IDWInterpolator(Interpolator):
         return rbf(xi, yi)
 
     @beartype.beartype
-    def interpolate(self, contact_orientations: pandas.DataFrame, map_data: MapData) -> numpy.ndarray:
+    def interpolate(self, map_data: MapData) -> numpy.ndarray:
         """
         Execute interpolation method (abstract method)
 
@@ -261,7 +279,7 @@ class IDWInterpolator(Interpolator):
 
         """
 
-        stratigraphic_orientations = self.setup_interpolation(contact_orientations, map_data)
+        stratigraphic_orientations = self.setup_interpolation(map_data)
         x, y = stratigraphic_orientations[["X", "Y"]].to_numpy()
         nx, ny, nz = stratigraphic_orientations[["nx", "ny", "nz"]].to_numpy()
         xi, yi = self.setup_grid(map_data)
@@ -271,4 +289,111 @@ class IDWInterpolator(Interpolator):
         ny_interp = self.interpolator(x, y, ny, xi, yi)
         nz_interp = self.interpolator(x, y, nz, xi, yi)
 
-        return numpy.array([nx_interp, ny_interp, nz_interp]).T
+        vecs = numpy.array([nx_interp, ny_interp, nz_interp]).T
+        vecs /= numpy.linalg.norm(vecs, axis=1)[:, None]
+
+        return vecs
+
+
+class DipInterpolator(Interpolator):
+    """
+
+
+    Args:
+        Interpolator(ABC): Derived from Abstract Base Class
+    """
+
+    def __init__(self):
+        """
+        Initialiser of for IDWInterpolator
+        """
+        self.interpolator_label = "IDWInterpolator"
+
+    def type(self):
+        """
+        Getter for subclass type label
+
+        Returns:
+            str: Name of subclass
+        """
+        return self.interpolator_label
+
+    @beartype.beartype
+    @abstractmethod
+    def setup_interpolation(self, map_data: MapData) -> Tuple[pandas.DataFrame, list, numpy.ndarray]:
+        """
+        Setup the interpolation method
+        """
+        contact_orientations = map_data.raw_data[1].copy()
+        contact_orientations['x'] = contact_orientations['geometry'].apply(lambda geom: geom.x)
+        contact_orientations['y'] = contact_orientations['geometry'].apply(lambda geom: geom.y)
+        x, y = contact_orientations[['x', 'y']].to_list()
+        dip = contact_orientations["dip"].to_list()
+
+        return x, y, dip
+
+    @beartype.beartype
+    def setup_grid(self, map_data: MapData) -> tuple[tuple[Any, Any] | Any, tuple[Any, Any] | Any]:
+        """
+        Setup the grid for interpolation
+
+        Args:
+            map_data (map2loop.MapData): a catchall so that access to all map data is available
+        """
+        # Define the desired cell size
+        cell_size = 0.05 * (map_data.bounding_box["maxx"] - map_data.bounding_box["minx"])
+
+        # Calculate the grid resolution
+        grid_resolution = round((map_data.bounding_box["maxx"] - map_data.bounding_box["minx"]) / cell_size)
+
+        # Generate the grid
+        xi = numpy.linspace(
+            map_data.bounding_box["minx"], map_data.bounding_box["maxx"], grid_resolution
+        )
+        yi = numpy.linspace(
+            map_data.bounding_box["miny"], map_data.bounding_box["maxy"], grid_resolution
+        )
+        return xi, yi
+
+    @beartype.beartype
+    def interpolator(self, x: Any, y: Any, ni: Any, xi: Any, yi: Any) -> numpy.ndarray:
+        """
+        Inverse Distance Weighting interpolation method
+
+        Args:
+            x (Any): x-coordinate of the point
+            y (Any): y-coordinate of the point
+            ni (Any): list or numpy.ndarray of values to interpolate
+            xi (Any): x-coordinate of the point where interpolation of ni is performed (grid point)
+            yi (Any): y-coordinate of the point where interpolation of ni is performed (grid point)
+
+        Returns:
+            Rbf: radial basis function object
+        """
+
+        rbf = Rbf(x, y, ni, function="linear")
+
+        return rbf(xi, yi)
+
+    @beartype.beartype
+    def interpolate(self, map_data: MapData) -> numpy.ndarray:
+        """
+        Execute interpolation method (abstract method)
+
+        Args:
+            contact_orientations (pandas.DataFrame): structural data with columns: 'X', 'Y', 'Z', 'dipDir/dipdir',
+            'dip', 'layer', 'name'
+            map_data (map2loop.MapData): a catchall so that access to all map data is available
+
+        Returns:
+            list: sorted list of unit names
+
+        """
+
+        x, y, dip = self.setup_interpolation(map_data)
+        xi, yi = self.setup_grid(map_data)
+
+        # interpolate each component of the normal vector nx, ny, nz
+        interpolated_dip = self.interpolator(x, y, dip, xi, yi)
+
+        return interpolated_dip

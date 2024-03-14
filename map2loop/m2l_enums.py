89910ea866ae2db3c5652c1db031bb0c52802781
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-03-14 14:54:34 +1100

    Update m2l_enums.py

--- map2loop/m2l_enums.py
+++ map2loop/m2l_enums.py
@@ -1,13 +1,6 @@
 from enum import IntEnum
 
 
-class Sampletype(IntEnum):
-    GEOLOGY = 0
-    STRUCTURE = 1
-    FAULT = 2
-    FOLD = 3
-    DTM = 4
-
 class Datatype(IntEnum):
     GEOLOGY = 0
     STRUCTURE = 1

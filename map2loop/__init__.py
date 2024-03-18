f7b2d6d739248c839a1fc7f1d7b8e024063815fa
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-03-18 12:54:55 +1100

    fix: can import interpolators

--- map2loop/__init__.py
+++ map2loop/__init__.py
@@ -1,5 +1,5 @@
 from .project import Project
-from .interpolator import Interpolator, IDWInterpolator
+from .interpolator import NormalVectorInterpolator, DipDipDirectionInterpolator
 
 __all__ = ["Project"]
 from .version import __version__

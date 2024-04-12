8886fce9892b983da5630f5b5edd75592019c270
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-04-12 13:10:19 +0930

    fix: new SampleType and StateType

--- map2loop/m2l_enums.py
+++ map2loop/m2l_enums.py
@@ -1,6 +1,16 @@
 from enum import IntEnum
 
 
+class SampleType(IntEnum):
+    GEOLOGY = 0
+    STRUCTURE = 1
+    FAULT = 2
+    FOLD = 3
+    FAULT_ORIENTATION = 4
+    CONTACT = 5
+    DTM = 6
+
+
 class Datatype(IntEnum):
     GEOLOGY = 0
     STRUCTURE = 1
@@ -30,3 +40,8 @@ class VerboseLevel(IntEnum):
     NONE = 0
     TEXTONLY = 1
     ALL = 2
+
+
+class StateType(IntEnum):
+    DATA = 0
+    SAMPLER = 1

69e6f72f83222d2b49948ecc64e1de7475391643
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-04-12 13:11:56 +0930

    chore: add import SampleSupervisor

--- map2loop/__init__.py
+++ map2loop/__init__.py
@@ -1,5 +1,6 @@
 from .project import Project
 from .version import __version__
 from .interpolators import NormalVectorInterpolator, DipDipDirectionInterpolator
+from .sample_storage import SampleSupervisor
 
 __all__ = ["Project"]

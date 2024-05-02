5a1a8b783137374e1d0cd2a2a11fdfe9414ebd26
RoyThomsonMonash <roy.thomson@monash.edu>
2024-05-02 17:02:33 +1000

    fix: Rename segNum to featureId

--- map2loop/project.py
+++ map2loop/project.py
@@ -615,7 +615,7 @@ class Project(object):
         contacts_data["easting"] = self.map_data.sampled_contacts["X"]
         contacts_data["northing"] = self.map_data.sampled_contacts["Y"]
         contacts_data["altitude"] = self.map_data.sampled_contacts["Z"]
-        contacts_data["segNum"] = self.map_data.sampled_contacts["featureId"]
+        contacts_data["featureId"] = self.map_data.sampled_contacts["featureId"]
         LPF.Set(self.loop_filename, "contacts", data=contacts_data)
 
         # Save fault trace information
@@ -627,7 +627,7 @@ class Project(object):
         faults_obs_data["easting"][0 : len(self.fault_samples)] = self.fault_samples["X"]
         faults_obs_data["northing"][0 : len(self.fault_samples)] = self.fault_samples["Y"]
         faults_obs_data["altitude"][0 : len(self.fault_samples)] = self.fault_samples["Z"]
-        faults_obs_data["segNum"][0 : len(self.fault_samples)] = self.fault_samples["featureId"]
+        faults_obs_data["featureId"][0 : len(self.fault_samples)] = self.fault_samples["featureId"]
         faults_obs_data["dipDir"][0 : len(self.fault_samples)] = numpy.nan
         faults_obs_data["dip"][0 : len(self.fault_samples)] = numpy.nan
         faults_obs_data["posOnly"][0 : len(self.fault_samples)] = 1
@@ -639,7 +639,7 @@ class Project(object):
         faults_obs_data["easting"][len(self.fault_samples) :] = self.fault_orientations["X"]
         faults_obs_data["northing"][len(self.fault_samples) :] = self.fault_orientations["Y"]
         faults_obs_data["altitude"][len(self.fault_samples) :] = self.fault_orientations["Z"]
-        faults_obs_data["segNum"][len(self.fault_samples) :] = self.fault_orientations["featureId"]
+        faults_obs_data["featureId"][len(self.fault_samples) :] = self.fault_orientations["featureId"]
         faults_obs_data["dipDir"][len(self.fault_samples) :] = self.fault_orientations["DIPDIR"]
         faults_obs_data["dip"][len(self.fault_samples) :] = self.fault_orientations["DIP"]
         faults_obs_data["posOnly"][len(self.fault_samples) :] = 0

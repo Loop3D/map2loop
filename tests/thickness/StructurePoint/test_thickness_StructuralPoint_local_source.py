d01d8ac2dd5fad91abf31c59078b8946ac7a794e
AngRodrigues <angela.rodrigues@monash.edu>
2024-05-03 16:50:10 +1000

    fix: update to solve the Pathlib

--- tests/thickness/StructurePoint/test_thickness_StructuralPoint_local_source.py
+++ tests/thickness/StructurePoint/test_thickness_StructuralPoint_local_source.py
@@ -200,9 +200,6 @@ proj = Project(
     fold_filename=os.path.join(path, "faults.shp"),
     structure_filename=os.path.join(path, "structures.shp"),
     dtm_filename=os.path.join(path, 'DEM.tif'),
-    clut_filename=os.path.join(
-        os.path.dirname(map2loop.__file__), "_datasets/clut_files/WA_clut.csv"
-    ),
     config_dictionary=config,
     clut_file_legacy=False,
     working_projection="EPSG:7854",

a41fe18f5a1bfb8bed9ea366891173d9ec946ba8
AngRodrigues <angela.rodrigues@monash.edu>
2024-05-06 15:01:01 +1000

    tests: make sure the plot_hamersley actually tests

--- tests/project/test_plot_hamersley.py
+++ tests/project/test_plot_hamersley.py
@@ -4,7 +4,7 @@ from map2loop.m2l_enums import VerboseLevel, Datatype
 from map2loop.sorter import SorterAlpha
 from map2loop.sampler import SamplerSpacing
 from pyproj.exceptions import CRSError
-
+import os
 
 bbox_3d = {
     "minx": 515687.31005864,
@@ -16,29 +16,21 @@ bbox_3d = {
 }
 loop_project_filename = "wa_output.loop3d"
 
+def test_project_execution():
+    proj = Project(
+        use_australian_state_data="WA",
+        working_projection="EPSG:28350",
+        bounding_box=bbox_3d,
+        clut_file_legacy=False,
+        verbose_level=VerboseLevel.NONE,
+        loop_project_filename=loop_project_filename,
+    )
+    proj.run_all(take_best=True)
+    assert proj is not None, "Plot Hamersley Basin failed to execute"
 
-def test_run_all_catches_all_errors():
-
-    try:
-        proj = Project(
-            use_australian_state_data="WA",
-            working_projection="EPSG:283350",
-            bounding_box=bbox_3d,
-            clut_file_legacy=False,
-            verbose_level=VerboseLevel.NONE,
-            loop_project_filename=loop_project_filename,
-        )
-        proj.set_sampler(Datatype.GEOLOGY, SamplerSpacing(200.0))
-        proj.set_sampler(Datatype.FAULT, SamplerSpacing(200.0))
-        proj.set_sorter(SorterAlpha())
-        proj.run_all(take_best=True)
-    except CRSError as crs_error:
-        print("Caught CRSError when running project:", crs_error)
-    except Exception as e:
-        print("Caught an exception:", e)
-    else:
-        print("No exceptions raised, test passed.")
-
+def test_file_creation():
+    expected_file = loop_project_filename
+    assert os.path.exists(expected_file), f"Expected file {expected_file} was not created"
 
 ###################################################################
 ## test if wrong crs will throw a crs error
@@ -48,7 +40,7 @@ def test_expect_crs_error():
     with pytest.raises(CRSError):
         Project(
             use_australian_state_data="WA",
-            working_projection="NittyGrittyEPSG",
+            working_projection="NittyGrittyCRS",
             bounding_box=bbox_3d,
             clut_file_legacy=False,
             verbose_level=VerboseLevel.NONE,

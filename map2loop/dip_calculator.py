4e126b9a8780553128c4bf852d19d91fb121a552
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-05-07 11:53:00 +0930

    feat: new Eigenanalysis dip calculator

--- map2loop/dip_calculator.py
+++ map2loop/dip_calculator.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Union
 import beartype
 from skspatial.objects import Plane
 import numpy
+import shapely
 
 
-class BaseDipCalculator(ABC):
+class DipCalculator(ABC):
     """
     Base Class of Sorter used to force structure of Sorter
 
@@ -16,7 +18,7 @@ class BaseDipCalculator(ABC):
         """
         Initialiser of for Sorter
         """
-        self.dip_calculator_label = "DipCalculatorBaseClass"
+        self.dip_calculator_label = "DipCalculator"
 
     def type(self):
         """
@@ -29,7 +31,7 @@ class BaseDipCalculator(ABC):
 
     @beartype.beartype
     @abstractmethod
-    def compute(self, points: list):
+    def compute(self, points: Union[list, numpy.ndarray]):
         """
         Execute dip calculator method (abstract method)
 
@@ -42,22 +44,20 @@ class BaseDipCalculator(ABC):
         pass
 
 
-class DipCalculator(BaseDipCalculator):
+class SvdDipCalculator(DipCalculator):
     """
-    DipCalculator class used to calculate dip of a plane
+    SvdDipCalculator class used to calculate dip of a plane
 
     Args:
-        DipCalculator (BaseDipCalculator): Derived from BaseDipCalculator
+        DipCalculator: Dip calculator dip base class
     """
 
     def __init__(self):
         """
         Initialiser of DipCalculator
         """
-        super().__init__()
-        self.dip_calculator_label = "DipCalculator"
-        self.data = None
-        self.dip = None
+
+        self.dip_calculator_label = "SvdDipCalculator"
 
     def type(self):
         """
@@ -69,9 +69,9 @@ class DipCalculator(BaseDipCalculator):
         return self.dip_calculator_label
 
     @beartype.beartype
-    def compute(self, points: list):
+    def compute(self, points: Union[list, numpy.ndarray]):
         """
-        Execute dip calculator method
+        Execute the Singular Value Decomposition (SVD) dip calculator method
 
         Args:
             points (list): list of points to compute dip
@@ -89,5 +89,60 @@ class DipCalculator(BaseDipCalculator):
         plane = Plane.best_fit([p1, p2, p3])
         normal_vector = plane.normal.round(4)
         dip = numpy.degrees(numpy.arccos(normal_vector[2]))
+
+        return dip
+
+
+class EigenDipCalculator(DipCalculator):
+    """
+    EigenDipCalculator class used to calculate dip of a plane
+
+    Args:
+        DipCalculator: Dip calculator dip base class
+    """
+
+    def __init__(self):
+        """
+        Initialiser of EigenDipCalculator
+        """
+        self.dip_calculator_label = "EigenDipCalculator"
+
+    def type(self):
+        """
+        Getter for subclass type label
+
+        Returns:
+            str: Name of subclass
+        """
+        return self.dip_calculator_label
+
+    @beartype.beartype
+    def compute(self, points: Union[list, numpy.ndarray]):
+        """
+        Execute the dip calculator using EigenAnalysis
+
+        Args:
+            points (list): list of points to compute dip
+
+        Returns:
+            list: dip value
+        """
+        if len(points) != 3:
+            raise ValueError("Dip calculation requires 3 points")
+
+        # Center the data by subtracting the mean of each column
+        centered_data = points - numpy.mean(points, axis=0)
+
+        # Calculate the covariance matrix
+        covariance = numpy.dot(centered_data.T, centered_data) / (points.shape[0] - 1)
+
+        # Perform eigenanalysis
+        eigenvalues, eigenvectors = numpy.linalg.eig(covariance)
+
+        # Get the normal vector (eigenvector corresponding to smallest eigenvalue)
+        normal_vector = eigenvectors[:, numpy.argmin(eigenvalues)]
+
+        # Calculate dip angle from normal vector
+        dip = numpy.degrees(numpy.arccos(normal_vector[2] / numpy.linalg.norm(normal_vector)))
 
         return dip

d2109145e7aa6d0d81df15b37c1c7d2aaa0a548e
AngRodrigues <angela.rodrigues@monash.edu>
2024-05-30 15:57:57 +1000

    fix: fix for altitude not being saved properly in LPF

--- map2loop/sampler.py
+++ map2loop/sampler.py
@@ -84,7 +84,7 @@ class SamplerDecimator(Sampler):
         data = spatial_data.copy()
         data["X"] = data.geometry.x
         data["Y"] = data.geometry.y
-        data["Z"] = map_data.get_value_from_raster_df(Datatype.DTM, data)["Z"]
+        map_data.get_value_from_raster_df(Datatype.DTM, data)
         data["layerID"] = geopandas.sjoin(
             data, map_data.get_map_data(Datatype.GEOLOGY), how='left'
         )['index_right']

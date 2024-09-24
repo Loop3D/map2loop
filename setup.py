1f7d97dd9b6d46269c40a8039832ed4a1749758e
AngRodrigues <angela.rodrigues@monash.edu>
2024-09-24 10:40:09 +1000

    fix: update setup.py to read dependencies file

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 """See pyproject.toml for project metadata."""
 
-from setuptools import setup
 import os
+from setuptools import setup
 
 package_root = os.path.abspath(os.path.dirname(__file__))
 
+# Get the version from the version.py file
 version = {}
 with open(os.path.join(package_root, "map2loop/version.py")) as fp:
     exec(fp.read(), version)
 version = version["__version__"]
-setup()
+
+# Read dependencies from dependencies.txt
+requirements_file = os.path.join(package_root, "dependencies.txt")
+with open(requirements_file, 'r') as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+setup(
+    name="map2loop",
+    install_requires=install_requires,
+    version=version,
+    license="MIT",
+    package_data={
+        # Include test files:
+        '': ['tests/*.py'],
+    },
+)

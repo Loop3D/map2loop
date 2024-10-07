a46b381ab1303ba29974fe883ce0c8a016c7dddc
Lachlan Grose <lachlan.grose@monash.edu>
2024-10-07 17:14:45 +1100

    Adding utils to get logger for file name and set logging levels

--- map2loop/__init__.py
+++ map2loop/__init__.py
@@ -1,2 +1,9 @@
+
+import logging
+loggers = {}
+ch = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)s: %(asctime)s: %(filename)s:%(lineno)d -- %(message)s")
+ch.setFormatter(formatter)
+ch.setLevel(logging.WARNING)
 from .project import Project
 from .version import __version__

48ee04c0a6c8a2bdd93d2a21f9442c31efecfab9
AngRodrigues <angela.rodrigues@monash.edu>
2024-12-11 15:13:20 +1100

    fix: revert to lst comprehension

--- map2loop/thickness_calculator.py
+++ map2loop/thickness_calculator.py
@@ -393,12 +393,8 @@ class InterpolatedStructure(ThicknessCalculator):
                 logger.warning(
                     f"Thickness Calculator InterpolatedStructure: Cannot calculate thickness between {stratigraphic_order[i]} and {stratigraphic_order[i + 1]}\n"
                 )
-                
-        geometries = []
-        for line in _lines:
-            geometries.append(line)
         
-        self.lines = geopandas.GeoDataFrame(geometry=geometries, crs = basal_contacts.crs)
+        self.lines = geopandas.GeoDataFrame(geometry=[line[0] for line in _lines], crs = basal_contacts.crs)
         self.lines['dip'] = _dips
         self._check_thickness_percentage_calculations(thicknesses)
         return thicknesses

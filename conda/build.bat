540ee7e2da2b3ca7136001b4c0c6e3e22db118f1
AngRodrigues <angela.rodrigues@monash.edu>
2025-01-07 13:01:24 +1100

    fix: windows debug

--- conda/build.bat
+++ conda/build.bat
@@ -1,3 +1,9 @@
+@echo on 
+echo "SP_DIR: %SP_DIR%"
+echo "RECIPE_DIR: %RECIPE_DIR%"
+
+dir %RECIPE_DIR%\..
+
 mkdir %SP_DIR%\map2loop
 copy %RECIPE_DIR%\..\dependencies.txt %SP_DIR%\map2loop\
 %PYTHON% -m pip install . --no-deps

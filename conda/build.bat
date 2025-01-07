516b3f71f0cf57bf614ea24573f04b71521e9e7e
AngRodrigues <angela.rodrigues@monash.edu>
2025-01-07 13:16:06 +1100

    win debug

--- conda/build.bat
+++ conda/build.bat
@@ -1,9 +1,1 @@
-@echo on 
-echo "SP_DIR: %SP_DIR%"
-echo "RECIPE_DIR: %RECIPE_DIR%"
-
-dir %RECIPE_DIR%\..
-
-mkdir %SP_DIR%\map2loop
-copy %RECIPE_DIR%\..\dependencies.txt %SP_DIR%\map2loop\
 %PYTHON% -m pip install . --no-deps

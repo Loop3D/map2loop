de9d5d001bf8bcc9ef823715e70a3777fccf6e51
AngRodrigues <angela.rodrigues@monash.edu>
2025-01-07 11:45:39 +1100

    change dir to site-packages

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p $PREFIX/share/map2loop
-cp $RECIPE_DIR/../dependencies.txt $PREFIX/share/map2loop/
+mkdir -p $SP_DIR/map2loop
+cp $RECIPE_DIR/../dependencies.txt $SP_DIR/map2loop/
 $PYTHON -m pip install . --no-deps

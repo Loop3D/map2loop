b5f36afb8bfa6623d9b178af6059118207aa68a6
AngRodrigues <angela.rodrigues@monash.edu>
2025-01-07 12:36:57 +1100

    enable debug for windows

--- conda/build.bat
+++ conda/build.bat
@@ -1,5 +1,9 @@
+@echo off
+
+echo %SP_DIR%
+echo %RECIPE_DIR%
+dir %RECIPE_DIR%\..
+
 mkdir %SP_DIR%\map2loop
 copy %RECIPE_DIR%\..\dependencies.txt %SP_DIR%\map2loop\
-copy %RECIPE_DIR%\..\LICENSE %SP_DIR%\map2loop\
-copy %RECIPE_DIR%\..\README.md %SP_DIR%\map2loop\
 %PYTHON% -m pip install . --no-deps

fd24d08da704f083e1783974d6f1b67ef4bb5dfb
AngRodrigues <angela.rodrigues@monash.edu>
2025-01-09 13:15:17 +1100

    fix: try conda build without the build scripts

--- conda/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p $SP_DIR/map2loop
-cp $RECIPE_DIR/../dependencies.txt $SP_DIR/map2loop/
-cp $RECIPE_DIR/../LICENSE $SP_DIR/map2loop/
-cp $RECIPE_DIR/../README.md $SP_DIR/map2loop/
-$PYTHON -m pip install . --no-deps

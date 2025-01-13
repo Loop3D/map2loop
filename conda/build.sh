0ffee6cd4e7c532fcb0689e6427b256955da335a
AngRodrigues <angela.rodrigues@monash.edu>
2025-01-13 12:17:15 +1100

    chore: add python to dep in conda build

--- conda/build.sh
+++ conda/build.sh
@@ -3,4 +3,4 @@ mkdir -p $SP_DIR/map2loop
 cp $RECIPE_DIR/../dependencies.txt $SP_DIR/map2loop/
 cp $RECIPE_DIR/../LICENSE $SP_DIR/map2loop/
 cp $RECIPE_DIR/../README.md $SP_DIR/map2loop/
-$PYTHON -m pip install . --no-deps
+$PYTHON -m pip install . 

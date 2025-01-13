c4cb6154e59eeb2a3be22e3c0a4c05b5982ec6da
AngRodrigues <angela.rodrigues@monash.edu>
2025-01-13 14:10:16 +1100

    chore: typo in conda build (#176)
    * chore: typo in conda build

--- conda/bld.bat
+++ conda/bld.bat
@@ -3,7 +3,11 @@ copy %RECIPE_DIR%\..\LICENSE %SP_DIR%\map2loop\
 copy %RECIPE_DIR%\..\README.md %SP_DIR%\map2loop\
 copy %RECIPE_DIR%\..\dependencies.txt %SP_DIR%\map2loop\
 <<<<<<< HEAD
+<<<<<<< HEAD
 %PYTHON% -m pip install . 
 =======
 %PYTHON% -m pip install . --no-deps
 >>>>>>> b33532b (fix: include dependencies in site-packages  - issue #169 (#170))
+=======
+%PYTHON% -m pip install . 
+>>>>>>> 9ca260f (chore: typo in conda build (#176))
--- conda/build.sh
+++ conda/build.sh
@@ -4,7 +4,11 @@ cp $RECIPE_DIR/../dependencies.txt $SP_DIR/map2loop/
 cp $RECIPE_DIR/../LICENSE $SP_DIR/map2loop/
 cp $RECIPE_DIR/../README.md $SP_DIR/map2loop/
 <<<<<<< HEAD
+<<<<<<< HEAD
 $PYTHON -m pip install . 
 =======
 $PYTHON -m pip install . --no-deps
 >>>>>>> b33532b (fix: include dependencies in site-packages  - issue #169 (#170))
+=======
+$PYTHON -m pip install . 
+>>>>>>> 9ca260f (chore: typo in conda build (#176))

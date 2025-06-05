ad0289fa6de5af9e311a9e21efa13e949ff8867a
rabii-chaarani <rabii.chaarani@monash.edu>
2025-06-05 10:58:32 +0930

    fix: support multiple search terms for faults

--- map2loop/mapdata.py
+++ map2loop/mapdata.py
@@ -995,9 +995,17 @@ class MapData:
         # crop
         faults = faults.loc[faults.geometry.length >= self.minimum_fault_length]
         
-        if config["structtype_column"] in self.raw_data[Datatype.FAULT]:               
+        if config["structtype_column"] in self.raw_data[Datatype.FAULT]:
             faults["FEATURE"] = self.raw_data[Datatype.FAULT][config["structtype_column"]]
-            faults = faults[faults["FEATURE"].astype(str).str.contains(config["fault_text"])]
+               # Check if the string contains commas indicating multiple substrings
+            if ',' in config["fault_text"]:
+                import re
+                fault_text = config["fault_text"].split(',')
+                search_terms = [term.strip().strip("'").strip('"') for term in fault_text]
+                escaped_terms = [re.escape(term) for term in search_terms]
+                # Combine the escaped terms using the pipe '|' symbol for alternation
+                fault_text_pattern = '|'.join(escaped_terms)
+                faults = faults[faults["FEATURE"].astype(str).str.contains(fault_text_pattern, case=False, regex=True, na=False)]
             if self.verbose_level > VerboseLevel.NONE:
                 if len(faults) < len(self.raw_data[Datatype.GEOLOGY]) and len(faults) == 0:
                     msg = f"Fault map reduced to 0 faults as structtype_column ({config['structtype_column']}) does not contains as row with fault_text \"{config['fault_text']}\""
